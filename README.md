b0f49509d091970154ccee97580fe031f2129f4f
PARK SUNG DONG <oraedoa@gmail.com>
2020-10-05 21:10:39 +0900

    [feat] querydsl ãaggregation

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 * 쿼리를 자바 코드로 작성이 가능하며, 문법오류를 컴파일 시점에 찾을 수 있다.
 * 동적쿼리 가능
 
+<BR>
+
 ## Q-Type 을 사용 방법 두가지
 ```java
 // 둘은 동일하다
@@ -20,21 +22,44 @@ final Member foundMember = queryFactory
         .fetchOne();
 ```
 
+<BR>
+
 ## Querydsl 에서 사용하는 jpql 검색 조건
 ```java
-member.username.eq("member1") // username = 'member1'
-member.username.ne("member1") //username != 'member1'
-member.username.eq("member1").not() // username != 'member1'
-member.username.isNotNull() //이름이 is not null
-member.age.in(10, 20) // age in (10,20)
-member.age.notIn(10, 20) // age not in (10, 20)
-member.age.between(10,30) //between 10, 30
-member.age.goe(30) // age >= 30
-member.age.gt(30) // age > 30
-member.age.loe(30) // age <= 30
-member.age.lt(30) // age < 30
-member.username.like("member%") //like 검색
-member.username.contains("member") // like ‘%member%’ 검색
-member.username.startsWith("member") //like ‘member%’ 검색
+member.username.eq("member1")           // username = 'member1'
+member.username.ne("member1")           //username != 'member1'
+member.username.eq("member1").not()     // username != 'member1'
+member.username.isNotNull()             // 이름이 is not null
+member.age.in(10, 20)                   // age in (10,20)
+member.age.notIn(10, 20)                // age not in (10, 20)
+member.age.between(10,30)               // between 10, 30 (10 과 30을 포함)
+member.age.goe(30)                      // age >= 30
+member.age.gt(30)                       // age > 30
+member.age.loe(30)                      // age <= 30
+member.age.lt(30)                       // age < 30
+member.username.like("member%")         // like 검색
+member.username.contains("member")      // like ‘%member%’ 검색
+member.username.startsWith("member")    // like ‘member%’ 검색
+```
 
+<BR>
+
+## Querydsl 결과조회
+```java
+fetch()         // 리스트 조회
+fetchOne()      // 단 건 조회
+                // |- 결과가 없으면 null
+                // |- 결과가 둘 이상이면, com.querydsl.core.NonUniqueResultExcetpion
+fetchFirst()    // limit(1).fetchOne()
+fetchResults()  // 페이징 정보 조회. total count 포함 ( .getTotal() )
+fetchCount()    // count 쿼리로 변경되어 count 수 조회
+```
+
+<BR>
+
+## Querydsl aggregation
+```java
+// 생략
+.groupBy(item.price)
+.having(item.price.gt(1000))
 ```
--- src/test/java/edu/pasudo123/study/demo/member/MemberTest.java
+++ src/test/java/edu/pasudo123/study/demo/member/MemberTest.java
@@ -1,5 +1,6 @@
 package edu.pasudo123.study.demo.member;
 
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import edu.pasudo123.study.demo.team.Team;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,7 +15,8 @@ import javax.persistence.EntityManager;
 
 import java.util.List;
 
-import static edu.pasudo123.study.demo.member.QMember.*;
+import static edu.pasudo123.study.demo.member.QMember.member;
+import static edu.pasudo123.study.demo.team.QTeam.team;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -26,11 +28,13 @@ class MemberTest {
     @Autowired
     private EntityManager em;
 
-    JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+    JPAQueryFactory queryFactory;
 
     @BeforeEach
     @DisplayName("Member 엔티티를 선행으로 삽입한다.")
     public void init() {
+        queryFactory = new JPAQueryFactory(em);
+
         Member member = Member.builder()
                 .username("PARK SUNG DONG")
                 .age(29)
@@ -84,7 +88,7 @@ class MemberTest {
 //                        member.username.eq("PARK SUNG DONG")
 //                                .and(member.age.eq(30)))
                         member.username.eq("PARK SUNG DONG"),
-                        member.age.eq(30)
+                        member.age.between(20, 31)
                 )
                 .fetchOne();
 
@@ -93,4 +97,109 @@ class MemberTest {
         assertThat(foundMember.getUsername()).isEqualTo("PARK SUNG DONG");
     }
 
+    @Test
+    @DisplayName("querydsl 을 통해 정렬 조회를 수행한다.")
+    public void querydslSortTest() {
+        // given
+        em.persist(Member.builder()
+                .username("SON")
+                .age(29)
+                .team(Team.builder().name("NEW TEAM").build())
+                .build());
+        em.persist(Member.builder()
+                .username("CHA")
+                .age(51)
+                .team(Team.builder().name("NEW TEAM").build())
+                .build());
+        em.persist(Member.builder()
+                .username("ABA")
+                .age(51)
+                .team(Team.builder().name("NEW TEAM").build())
+                .build());
+        em.flush();
+        em.clear();
+
+        // when
+        final List<Member> results = queryFactory
+                .selectFrom(member)
+                .where(member.age.lt(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsFirst())
+                .fetch();
+
+        // then
+        assertThat(results.size()).isNotZero();
+    }
+
+    @Test
+    @DisplayName("querydsl 을 통해 페이징 조회를 수행한다.")
+    public void querydslPagingTest() {
+        final List<Member> members = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(0)  // offset 은 0부터
+                .limit(1)
+                .fetch();
+
+        // then
+        // order by member.username desc limit ? offset ?
+        assertThat(members.size()).isNotZero();
+    }
+
+    @Test
+    @DisplayName("querydsl 을 이용하여 집함 함수를 이용한다.")
+    public void querydslAggregationTest() {
+
+        // given
+        em.persist(Member.builder()
+                .username("ABA")
+                .age(51)
+                .team(Team.builder().name("NEW TEAM").build())
+                .build());
+        em.flush();
+        em.clear();
+
+        // when
+        final List<Tuple> tuples = queryFactory
+                .select(
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                .fetch();
+
+        // then
+        // 여러 개의 타입을 꺼내올 수 있다.
+        Tuple tuple = tuples.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(2);
+        assertThat(tuple.get(member.age.sum())).isGreaterThan(0);
+    }
+    
+    @Test
+    @DisplayName("팀의 이름과 각 팀의 연령을 구하기")
+    public void querydslGroupTest() {
+
+        // given
+        em.persist(Member.builder()
+                .username("ABA")
+                .age(51)
+                .team(Team.builder().name("YES TEAM").build())
+                .build());
+        em.flush();
+        em.clear();
+
+        // when
+        List<Tuple> tuples = queryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .fetch();
+
+        // then
+        Tuple teamA = tuples.get(0);
+        Tuple teamB = tuples.get(1);
+    }
 }
